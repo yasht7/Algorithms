f82c6d19ea38ec00ed30e6e35ede6caeed3c3558
Yashaswi Tamta <yashaswitamta@gmail.com>
2017-05-17 11:31:12 -0700

    Updated Test case

--- 1/Difference_pairs.py
+++ 1/Difference_pairs.py
@@ -1,15 +1,15 @@
 '''
-Given an array arr of distinct integers and a nonnegative integer k, write a function findPairsWithGivenDifference that returns an array of all pairs [x,y] in arr, such that x - y = k. If no such pairs exist, return an empty array.
-
+Given an array arr of distinct integers and a nonnegative integer k, write a function findPairsWithGivenDifference 
+that returns an array of all pairs [x,y] in arr, such that x - y = k. If no such pairs exist, return an empty array.
 '''
 
 import copy
 def findPairsWithGivenDifference(arr, k):
     output = []
-  
+    
     if len(arr) < 2:
         return output
-  
+    
     arr2 = copy.copy(arr)
 
     arr2.sort()
@@ -29,5 +29,6 @@ def findPairsWithGivenDifference(arr, k):
             start +=1
             end +=1
     return output
-    
+
+# Test case
 print(findPairsWithGivenDifference([0, -1, 2, 2, 1],1))
